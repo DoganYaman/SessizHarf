724643dcefa52e92ea602668de4a7cd65f2d3925
Dogan Yaman <yamandogan963@gmail.com>
2024-01-12 23:45:19 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace sessiz_harf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir cümle giriniz : ");
+            string ifade = Console.ReadLine();
+            SessizHarYanyana(ifade);
+        }
+
+        static void SessizHarYanyana(string ifade)
+        {
+            string[] kelimeDizisi = ifade.Split(' ');
+            char[] sessizHarfler = new char[] {'b','c','ç','d','f','g','ğ','h','j','k','l','m','n','p','r','s','ş','t','v','w','y','z','q'};
+            bool durum;
+            Console.Write("Çıktı : ");
+            foreach (string kelime in kelimeDizisi)
+            {
+                durum = false;
+                for (int i = 0; i < kelime.Length -1; i++)
+                {
+                    if(sessizHarfler.Contains(kelime[i]) && sessizHarfler.Contains(kelime[i+1]))
+                    {
+                        durum = true;
+                        break;
+                    }
+                }
+
+                Console.Write(durum + " ");
+            }
+
+        }
+    }
+}
